a4ade69c72296bf2fa0dd220816ddb5ec612e6d7
KobeHill68 <kobehill68@gmail.com>
2021-02-11 14:57:31 -0600

    styling the pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,15 @@ import '../App.css'
 export default class Contact extends Component {
     render() {
         return (
-            <div className='page-footer'>
-                <h4>Github: Link to github</h4>
-                <h4>LinkedIn: </h4>
-                <h4>Email: </h4>
-
+            <div className='container mt-5'>
+                <h2 className='mt-3 mb-3'> Contact Me</h2>
+                <div className="row">
+                    <div className="col">
+                        <h4>Github: Link to github</h4>
+                        <h4>LinkedIn: </h4>
+                        <h4>Email: </h4>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,16 +6,15 @@ import '../App.css'
 export default class Projects extends Component {
     render() {
         return (
-            <div className='container'>
+            <div className='container mt-5'>
                 <div className='row'>
                     <div className='col-md-4 well'>
                         <div className='project'>
-                            <h3>Project 1</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan, erat maximus suscipit congue,
-                            diam orci rutrum nisl, vel posuere odio velit semper nibh. Cras vestibulum vestibulum turpis, at consequat
-                            ligula rutrum eget. Praesent et pretium metus, vitae egestas mi. Vestibulum at nisl gravida, vehicula elit at,
-                            mattis sem. Pellentesque ante nisl, laoreet eget molestie vitae, varius quis nulla. Mauris sed ullamcorper massa.
-                            Vestibulum libero sem, elementum ut vehicula vel, placerat at elit.</p>
+                            <h3>Travel Planner</h3>
+                            <p>We all love to travel, it is a big part of are lives. But trying to plan a
+                            trip is a hassel. So we set out to making travel planning easy. Our Web Application does
+                            just that, you can say our web site is a one stop shop.</p>
+                            <a href="https://benjdg.github.io/Travel_App/" class="btn btn-primary">Travel Planner</a>
                         </div>
                     </div>
 
